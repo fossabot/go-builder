62d5a65db2a9c5c0c5c5ae09aa6ad7def12164c3
demaggus83 <demaggus83@outlook.de>
2019-07-24 14:54:01 +0000

    + fix build

--- _example/build.sh
+++ _example/build.sh
@@ -4,4 +4,8 @@ UPX="9"
 GIT_COMMIT=$(git rev-list -1 HEAD) 
 VERSION=$(git describe --all --exact-match `git rev-parse HEAD` | grep tags | sed 's/tags\///')
 
+if [ -z ${VERSION}]; then
+    VERSION="unknown"
+fi
+
 go-builder ${ENTRY} -git ${GIT_COMMIT} -version ${VERSION} -upx ${UPX}
--- build.sh
+++ build.sh
@@ -2,4 +2,10 @@
 GIT_COMMIT=$(git rev-list -1 HEAD) 
 VERSION=$(git describe --all --exact-match `git rev-parse HEAD` | grep tags | sed 's/tags\///')
 
+if [ -z ${VERSION}]; then
+    VERSION="unknown"
+fi
+
+echo ${VERSION}
+
 go run cmd/builder/main.go -- cmd/builder/main.go -git ${GIT_COMMIT} -version ${VERSION} -upx 9
