811bda1fd0a1ae385a96a4ef316bb7ff1cbe8d91
demaggus83 <demaggus83@outlook.de>
2019-09-13 14:53:16 +0000

    bla

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/demaggus83/go-builder
 
-go 1.12
+go 1.13
--- pkg/builder/build.go.1.12.go
+++ pkg/builder/build.go.1.12.go
@@ -8,8 +8,6 @@ func (b *Build) getSpecialArgs(meta *Meta, distFilePath, entryFile string) []str
 	return []string{}
 }
 
-// -tags tag,list : a space-separated list of build tags to consider satisfied during the build...
-// todo: may need to modify when go 1.14 is released https://github.com/golang/go/issues/26492
 func (b *Build) getTags() []string {
 	return []string{"-tags", strings.Join(tags, " ")}
 }
